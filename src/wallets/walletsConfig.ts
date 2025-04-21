a5c108243b80b259ac24889bffb4d18b9b6fc1dc
Ruveyda <thisisruveyda@gmail.com>
2025-04-21 22:47:11 +0330

    feat: add hana wallet

--- src/wallets/walletsConfig.ts
+++ src/wallets/walletsConfig.ts
@@ -4,11 +4,13 @@ import { lobstrConfig } from './configs/lobstrConfig';
 import { albedoConfig } from './configs/albedoConfig';
 import { freighterConfig } from './configs/freighterConfig';
 import { SupportedWallets, WalletInterface } from '../types';
+import { HanaConfig } from './configs/hanaConfig';
 
 export const walletsConfig: Record<SupportedWallets, WalletInterface> = {
   [SupportedWallets.Freighter]: freighterConfig,
   [SupportedWallets.Rabet]: rabetConfig,
+  [SupportedWallets.Hana]: HanaConfig,
+  [SupportedWallets.Lobstr]: lobstrConfig,
   [SupportedWallets.Albedo]: albedoConfig,
   [SupportedWallets.Xbull]: xBullConfig,
-  [SupportedWallets.Lobstr]: lobstrConfig,
 };
